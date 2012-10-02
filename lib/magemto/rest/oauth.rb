30373780192925bfcd5d729a8a6e0a6966be0fe6
Charles D'Angelo <c.g.dangelo@gmail.com>
2012-10-02 07:55:12 -0400

    Add poorly written specs for building OAuth objects

--- lib/magemto/rest/oauth.rb
+++ lib/magemto/rest/oauth.rb
@@ -1,9 +1,12 @@
 module Magemto
   module REST
     class OAuth
-      attr_accessor :name, :key, :secret
+      attr_accessor :name, :key, :secret, :url
+      attr_writer :oauth
 
-      def initialize
+      def initialize(name, key, secret, url)
+        @name, @key, @secret, @url = name, key, secret, url
+        @oauth = OAuth::Consumer.new(@key, @secret, @url)
       end
     end
   end
--- spec/magemto/rest/oauth_spec.rb
+++ spec/magemto/rest/oauth_spec.rb
@@ -3,27 +3,34 @@ require "magemto/rest/oauth"
 
 describe "Magemto::REST::OAuth" do
   before(:all) do
-    @oauth = Magemto::REST::OAuth.new
+    @oauth = Magemto::REST::OAuth.new("magemto","zjlbw63tvqcn7f43pt2xedzabi42ridj","5qx7cwen4exziq58oieftu3qirwzp8vu")
   end
 
-  it "should instantiate without arguments" do
-    lambda { Magemto::REST::OAuth.new }.should_not raise_error(ArgumentError)
+  it "should not instantiate without arguments" do
+    lambda { Magemto::REST::OAuth.new }.should raise_error(ArgumentError)
   end
   
   context "setting class variables" do
     it "should set name" do
       @oauth.name = "magemto"
-      @oauth.name.should match "magemto"
+      @oauth.instance_eval{ @name }.should match "magemto"
     end
 
     it "should set key" do
       @oauth.key = "zjlbw63tvqcn7f43pt2xedzabi42ridj"
-      @oauth.key.should match "zjlbw63tvqcn7f43pt2xedzabi42ridj"
+      @oauth.instance_eval{ @key }.should match "zjlbw63tvqcn7f43pt2xedzabi42ridj"
     end
 
     it "should set secret" do
       @oauth.secret = "5qx7cwen4exziq58oieftu3qirwzp8vu"
-      @oauth.secret.should match "5qx7cwen4exziq58oieftu3qirwzp8vu"
+      @oauth.instance_eval{ @secret }.should match "5qx7cwen4exziq58oieftu3qirwzp8vu"
+    end
+  end
+
+  context "setting up oauth object" do
+    require 'oauth'
+    it "should have consumer" do
+      @oauth.instance_eval{ @oauth }.should be_instance_of(OAuth::Consumer)
     end
   end
 end
